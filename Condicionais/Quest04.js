031eb36e538e30bee30af1dcf6881707ba91ea3f
Matheus Cordeiro Dutra Nunes <mdutranunes@gmail.com>
2023-04-23 11:38:18 -0300

    Questão04 Condicionais

---
+++ Condicionais/Quest04.js
@@ -0,0 +1,12 @@
+//4. Faça um algoritmo que leia dois valores inteiros A e B se os valores forem iguais deverá se somar os dois, caso contrário
+//multiplique A por B.
+
+let a = Number(prompt('Digite um número: '))
+let b = Number(prompt('Digite um número: '))
+
+if (a == b) {
+  console.log(a + b)
+}
+else {
+  console.log(a * b)
+}
